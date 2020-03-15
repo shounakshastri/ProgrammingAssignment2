826ef01a526a8f20b86c876993bc94f95226f090
shounakshastri <shounak.shastri@gmail.com>
2020-03-15 10:22:39 +0530

    This commit contains the code for calculating the inverse of a matrix and to check if the original matrix exists in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a matrix which should be stored and used for calculating the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    ## This is a second function nested inside the makeCacheMatrix(). This function is used to initialize 'inv'
+    ## to NULL. Here we use <<- operator to assign value of y to x as the scopes or environments of the two 
+    ## variables are different4
+    x <<- y
+    inv <<- NULL 
+  }
+  get <- function() x
+  set_inv <- function(inverse) inv <<- inverse
+  get_inv <- function() inv
+  list(set = set, get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
+  
 
 }
 
 
-## Write a short comment describing this function
-
+## This function first checks the matrix. If it finds the matrix in the cache, then it takes the inverse from the
+## cache, thus making the program faster. If not, then it calculates the inverse using the solve() function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  inv <- x$get_inv()
+  if(!is.null(inv)) { ## Check if the matrix is stored in the cache
+    message("getting cached data")
+    return(inv)## Return a matrix that is the inverse of 'x'
+    
+  }
+  data <- x$get()
+  inv <- solve(data, ...)  ## Calculate the inverse of the matrix because it is not in the cache.
+  x$set_inv(inv)
+  inv
 }
